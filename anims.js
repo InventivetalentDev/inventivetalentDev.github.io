ef1069be8c00ad1b4ac8a83b79ee2ce69a7d5346
inventivetalent <mail@inventivetalent.org>
2018-10-01 20:17:17 +0200

    move animation code that's more important to a separate script to load earlier

---
+++ anims.js
@@ -0,0 +1,30 @@
+(function () {
+    document.getElementById("separator_line_right").addEventListener("animationend", function () {
+        setTimeout(function () {
+            $(".upper-content").addClass("visible");
+        }, 100);
+        $(".slideshow-item")[0].addEventListener("transitionend", function (event) {
+            $(".slideshow").slick('slickGoTo', 0, true);
+        }, false);
+        setTimeout(function () {
+            $(".slideshow-content").addClass("visible");
+            $(".slideshow-item").addClass("visible");
+
+            $(".avatar-img").addClass("visible");
+            // $(".fadeInBtn").addClass("visible");
+        }, 400);
+        setTimeout(function () {
+            $(".lower-content").addClass("visible");
+        }, 600);
+        setTimeout(function () {
+            $(".contact-content").addClass("visible");
+        }, 800);
+        setTimeout(function () {
+            $(".embed-content").addClass("visible");
+            $(".embed").fadeIn();
+        }, 1000);
+        setTimeout(function () {
+            $("footer").addClass("visible");
+        }, 1200);
+    }, false);
+})();
